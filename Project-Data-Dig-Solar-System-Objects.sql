74c21d7adad73c5005472c5e70536316f18be08b
meganaware <126425692+meganaware@users.noreply.github.com>
2023-03-22 04:38:46 -0500

    Update Project-Data-Dig-Solar-System-Objects.sql

--- Project-Data-Dig-Solar-System-Objects.sql
+++ Project-Data-Dig-Solar-System-Objects.sql
@@ -2,18 +2,13 @@
 Khan Academy Intro to SQL: Querying and Managing Data 
 Lesson 2: More Advanced SQL Queries 
 Project: Data Dig
-*/
-
-/*We’ve curated a set of interesting data sets for you: Top movies, Top countries by population, 
-Solar system objects by size, Marvel characters, Furniture store sales, Earned KA badges, 
-Winston's donut logs, Card game results, and NFL draft picks. 
-Pick one of those data sets or create a data set like that, and use advanced SELECT queries to 
-discover things about the data. What sort of questions might one have about that data, like if 
-they were using it for an app or a business idea? Here are some ideas:
-What are average, max, and min values in the data?
-What about those numbers per category in the data (using HAVING)?
-What ways are there to group the data values that don’t exist yet (using CASE)?
-What interesting ways are there to filter the data (using AND/OR)? 
+  We’ve curated a set of interesting data sets for you: Top movies, Top countries by population, Solar system objects by size, Marvel characters, Furniture store sales, Earned KA badges, Winston's donut logs, Card game results, and NFL draft picks. 
+  Pick one of those data sets or create a data set like that, and use advanced SELECT queries to discover things about the data. 
+  What sort of questions might one have about that data, like if they were using it for an app or a business idea? Here are some ideas:
+  What are average, max, and min values in the data?
+  What about those numbers per category in the data (using HAVING)?
+  What ways are there to group the data values that don’t exist yet (using CASE)?
+  What interesting ways are there to filter the data (using AND/OR)? 
 */
 
 /* Solar system objects
