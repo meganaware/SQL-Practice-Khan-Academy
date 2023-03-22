8d8e8736ea6b7611afad3b93696bd16eb356a135
meganaware <126425692+meganaware@users.noreply.github.com>
2023-03-22 04:05:30 -0500

    Update and rename Practice-Solutions-challenges.sql to Challenges-SQL-Basics.sql

--- Challenges-SQL-Basics.sql
+++ Challenges-SQL-Basics.sql
@@ -1,4 +1,8 @@
-/*Khan Academy SQL Course Challenges*/
+/*
+Khan Academy Intro to SQL: Querying and Managing Data 
+Lesson 1: SQL Basics 
+Course Challenges
+*/
 
 /*CHALLENGE: Book list database
 What are your favorite books? You can make a database table to store them in! 
@@ -62,37 +66,6 @@ INSERT INTO todo_list VALUES (4, "Read homework", 60);
 
 SELECT SUM(minutes) FROM todo_list;
 
-
-/*CHALLENGE: Design a store
-Create your own store! Your store should sell one type of things, like clothing or bikes, whatever you want
-store to specialize in. You should have a table for all the items in your store, and at least 5 columns 
-for the kind of data you think you'd need to store. You should sell at least 15 items, and use select 
-statements to order your items by price and show at least one statistic about the items.
- */
- 
-CREATE TABLE clothing (id INTEGER PRIMARY KEY, item_type TEXT, quantity INTEGER, price INTEGER, size TEXT);
-
-INSERT INTO clothing VALUES (1, "Dress", 10, 54.00, "L");
-INSERT INTO clothing VALUES (2, "Skirt", 5, 64.00, "L");
-INSERT INTO clothing VALUES (3, "Shorts", 50, 4.00, "L");
-INSERT INTO clothing VALUES (4, "Pants", 6, 104.00, "L");
-INSERT INTO clothing VALUES (5, "Jeans", 60, 298.00, "L");
-INSERT INTO clothing VALUES (6, "Leggings", 7, 33.00, "L");
-INSERT INTO clothing VALUES (7, "Socks", 77, 5.00, "L");
-INSERT INTO clothing VALUES (8, "Underware", 8, 4.99, "L");
-INSERT INTO clothing VALUES (9, "Bras", 80, 75.00, "L");
-INSERT INTO clothing VALUES (10, "Bathing Suit", 200, 54.00, "L");
-INSERT INTO clothing VALUES (11, "Bikini", 100, 150.00, "L");
-INSERT INTO clothing VALUES (12, "Shoes", 1000, 500.00, "L");
-INSERT INTO clothing VALUES (13, "Scarf", 66, 99.00, "L");
-INSERT INTO clothing VALUES (14, "Shirt", 88, 54.00, "L");
-INSERT INTO clothing VALUES (15, "Tank Top", 99, 54.00, "L");
-
-SELECT * FROM clothing 
-ORDER BY price;
-
-SELECT SUM(quantity) FROM clothing;
- 
  
 /*CHALLENGE: Karaoke song selector
 We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing.
