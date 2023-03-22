bb13063e44e77f0c310dbf2039e22de50efbf210
meganaware <126425692+meganaware@users.noreply.github.com>
2023-03-22 04:07:29 -0500

    Create Challenges-More-Advanced-SQL-Queries.sql

---
+++ Challenges-More-Advanced-SQL-Queries.sql
@@ -0,0 +1,250 @@
+/*
+Khan Academy Intro to SQL: Querying and Managing Data 
+Lesson 2: More Advanced SQL Queries 
+Course Challenges
+*/
+
+ 
+/*CHALLENGE: Karaoke song selector
+We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing.
+*/
+
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    artist TEXT,
+    mood TEXT,
+    duration INTEGER,
+    released INTEGER);
+    
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("Bohemian Rhapsody", "Queen", "epic", 60, 1975);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("Let it go", "Idina Menzel", "epic", 227, 2013);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("I will survive", "Gloria Gaynor", "epic", 198, 1978);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("Twist and Shout", "The Beatles", "happy", 152, 1963);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("La Bamba", "Ritchie Valens", "happy", 166, 1958);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("I will always love you", "Whitney Houston", "epic", 273, 1992);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("Sweet Caroline", "Neil Diamond", "happy", 201, 1969);
+INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("Call me maybe", "Carly Rae Jepsen", "happy", 193, 2011);
+    
+/*Select all the song titles.
+*/
+
+SELECT title FROM songs;
+
+/*Show the titles of the songs that have an 'epic' mood or a release date after 1990.
+*/
+
+SELECT title FROM songs WHERE mood = "epic" OR released > 1990;
+
+/*Show the titles of songs that are 'epic', and released after 1990, and less than 4 minutes long.
+Note that the duration column is measured in seconds.
+*/
+
+SELECT title FROM songs WHERE mood = "epic" AND released > 1990 AND duration < 240;
+ 
+
+/*CHALLENGE: Playlist maker
+We've created a database of songs and artists, and you'll make playlists 
+from them in this challenge. 
+*/
+
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    country TEXT,
+    genre TEXT);
+
+INSERT INTO artists (name, country, genre)
+    VALUES ("Taylor Swift", "US", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Led Zeppelin", "US", "Hard rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("ABBA", "Sweden", "Disco");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Queen", "UK", "Rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Celine Dion", "Canada", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Meatloaf", "US", "Hard rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Garth Brooks", "US", "Country");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Shania Twain", "Canada", "Country");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Rihanna", "US", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Guns N' Roses", "US", "Hard rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Gloria Estefan", "US", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Bob Marley", "Jamaica", "Reggae");
+
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist TEXT,
+    title TEXT);
+
+INSERT INTO songs (artist, title)
+    VALUES ("Taylor Swift", "Shake it off");
+INSERT INTO songs (artist, title)
+    VALUES ("Rihanna", "Stay");
+INSERT INTO songs (artist, title)
+    VALUES ("Celine Dion", "My heart will go on");
+INSERT INTO songs (artist, title)
+    VALUES ("Celine Dion", "A new day has come");
+INSERT INTO songs (artist, title)
+    VALUES ("Shania Twain", "Party for two");
+INSERT INTO songs (artist, title)
+    VALUES ("Gloria Estefan", "Conga");
+INSERT INTO songs (artist, title)
+    VALUES ("Led Zeppelin", "Stairway to heaven");
+INSERT INTO songs (artist, title)
+    VALUES ("ABBA", "Mamma mia");
+INSERT INTO songs (artist, title)
+    VALUES ("Queen", "Bicycle Race");
+INSERT INTO songs (artist, title)
+    VALUES ("Queen", "Bohemian Rhapsody");
+INSERT INTO songs (artist, title)
+    VALUES ("Guns N' Roses", "Don't cry");
+    
+/*Select the title of all the songs by the artist named 'Queen'.
+*/
+
+SELECT title FROM songs WHERE artist = "Queen";
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+/*Select the name of all of the artists from the 'Pop' genre.
+*/
+
+Select name FROM artists WHERE genre = "Pop";
+
+/*Select the title of all the songs from the 'Pop' artists. It should 
+use IN on a nested subquery that's based on your previous query.
+*/
+
+SELECT title FROM songs 
+WHERE artist IN (
+    SELECT name FROM artists 
+    WHERE genre = "Pop");
+    
+
+/*CHALLENGE: Wordiest author
+We've created a database of a few popular authors and their books, with word counts 
+for each book.
+*/
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author TEXT,
+    title TEXT,
+    words INTEGER);
+    
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Philosopher's Stone", 79944);
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Chamber of Secrets", 85141);
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Prisoner of Azkaban", 107253);
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Goblet of Fire", 190637);
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Order of the Phoenix", 257045);
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Half-Blood Prince", 168923);
+INSERT INTO books (author, title, words)
+    VALUES ("J.K. Rowling", "Harry Potter and the Deathly Hallows", 197651);
+
+INSERT INTO books (author, title, words)
+    VALUES ("Stephenie Meyer", "Twilight", 118501);
+INSERT INTO books (author, title, words)
+    VALUES ("Stephenie Meyer", "New Moon", 132807);
+INSERT INTO books (author, title, words)
+    VALUES ("Stephenie Meyer", "Eclipse", 147930);
+INSERT INTO books (author, title, words)
+    VALUES ("Stephenie Meyer", "Breaking Dawn", 192196);
+    
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "The Hobbit", 95022);
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "Fellowship of the Ring", 177227);
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "Two Towers", 143436);
+INSERT INTO books (author, title, words)
+    VALUES ("J.R.R. Tolkien", "Return of the King", 134462);
+
+/*Select all the authors who have written more than 1 million words, using GROUP BY and HAVING. Your results table should 
+include the 'author' and their total word count as a 'total_words' column.
+*/
+
+SELECT author, SUM(words) AS total_words 
+FROM books GROUP BY author 
+HAVING total_words > 1000000;
+
+ /*Select all the authors that write more than an average of 150,000 words per book. 
+ Your results table should include the 'author' and average words as an 'avg_words' 
+ column.
+ */
+
+ SELECT author, AVG(words) AS avg_words 
+ FROM books 
+ GROUP BY author
+ HAVING avg_words > 150000;
+ 
+ 
+/*CHALLENGE: Gradebook
+We've created a database to track student grades, with their name, number grade, 
+and what percent of activities they've completed. 
+*/
+
+CREATE TABLE student_grades (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    number_grade INTEGER,
+    fraction_completed REAL);
+    
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winston", 90, 0.805);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winnefer", 95, 0.901);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winsteen", 85, 0.906);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Wincifer", 66, 0.7054);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winster", 76, 0.5013);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winstonia", 82, 0.9045);
+
+/*Select all of the rows, and display the name, number_grade, and percent_completed, 
+which you can compute by multiplying and rounding the fraction_completed column.
+*/
+
+SELECT name, number_grade, ROUND(fraction_completed * 100) AS percent_completed 
+ FROM student_grades; 
+
+/*The goal is a table that shows how many students have earned which letter_grade. 
+You can output the letter_grade by using CASE with the number_grade column, outputting 
+'A' for grades > 90, 
+'B' for grades > 80, 
+'C' for grades > 70, 
+and 'F' otherwise. 
+Then you can use COUNT with GROUP BY to show the number of students with each of those grades.
+*/
+
+SELECT COUNT(*),
+    CASE
+        WHEN number_grade > 90 THEN "A"
+        WHEN number_grade > 80 THEN "B"
+        WHEN number_grade > 70 THEN "C"
+        ELSE "F"
+    END AS "letter_grade"
+FROM student_grades
+GROUP BY letter_grade
+
