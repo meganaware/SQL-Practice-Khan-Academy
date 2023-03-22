aeece2c52407edb2df011c638e2852462f680bea
meganaware <126425692+meganaware@users.noreply.github.com>
2023-03-22 05:31:10 -0500

    Update Project-Design-A-Store-Database.sql

--- Project-Design-A-Store-Database.sql
+++ Project-Design-A-Store-Database.sql
@@ -1,12 +1,11 @@
-/*
-Khan Academy Intro to SQL: Querying and Managing Data 
-Lesson 1: SQL Basics 
-Project: Design a store database
-Create your own store! Your store should sell one type of things, like clothing or bikes, whatever you want your store to specialize
-in. You should have a table for all the items in your store, and at least 5 columns for the kind of data you think you'd need to store.
-You should sell at least 15 items, and use select statements to order your items by price and show at least one statistic 
-about the items.
-*/
+/*Khan Academy Intro to SQL: Querying and Managing Data 
+Lesson 1: SQL Basics*/ 
+
+/*PROJECT: Design a store database
+  Create your own store! Your store should sell one type of things, like clothing or bikes, whatever you want your store to specialize in. 
+  You should have a table for all the items in your store, and at least 5 columns for the kind of data you think you'd need to store.
+  You should sell at least 15 items, and use select statements to order your items by price and show at least one statistic about the items.*/
+
 
 CREATE TABLE shoes(
 id INTEGER PRIMARY KEY,
